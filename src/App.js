d7322070d3e3ffe31dd8febacb9dbe05b88c451e
YounghwanShin <yong060309@gmail.com>
2024-12-24 22:15:59 +0900

    UpdateService: add image

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/owner" element={<OwnerView />} />
-        {/* ... */}
       </Routes>
     </HashRouter>
   );
