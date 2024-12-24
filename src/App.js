3764422e77b1251bb76f60f51b162e44e7dfa06a
YounghwanShin <yong060309@gmail.com>
2024-12-24 22:03:19 +0900

    release

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import OwnerView from './pages/OwnerView';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/owner" element={<OwnerView />} />
+        {/* ... */}
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
